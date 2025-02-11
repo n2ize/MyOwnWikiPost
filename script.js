d14291e73d3f912adc3da24bafbcfbb25340f9c8
n2ize <niki.ans@gmx.de>
2025-02-11 09:29:42 +0100

    First Commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function changeImageBase(){
+    var img = document.getElementById('Image');
+    img.src = 'Kayn_Render.webp';
+}
+
+function changeImageShadowAssassin(){
+    var img = document.getElementById('Image');
+    img.src = 'Kayn_ShadowAssassin_Render.webp';
+}
+
+function changeImageDarkin(){
+    var img = document.getElementById('Image');
+    img.src = 'Kayn_Rhaast_Render.webp';
+}
